08d13bf46cd6b333c3d6d5e31bce7c0114869b66
Luca Weist <lweist3317@gmail.com>
2022-06-01 19:39:19 +0200

    some hope?

--- signallibp2p/handshake.go
+++ signallibp2p/handshake.go
@@ -2,14 +2,13 @@ package signallibp2p
 
 import (
 	"context"
-	"errors"
 
 	"github.com/Luca3317/libsignalcopy/keys/prekey"
 	"github.com/Luca3317/libsignalcopy/logger"
 	"github.com/Luca3317/libsignalcopy/protocol"
-	"github.com/Luca3317/libsignalcopy/serialize"
 	"github.com/Luca3317/libsignalcopy/session"
 	"github.com/Luca3317/libsignalcopy/util/retrievable"
+	pool "github.com/libp2p/go-buffer-pool"
 )
 
 func (s *signalSession) Handshake(ctx context.Context) (err error) {
@@ -59,18 +58,6 @@ func (s *signalSession) Handshake(ctx context.Context) (err error) {
 			return err
 		}
 
-		// test
-		rm, err := protocol.NewPreKeySignalMessageFromBytes(message.Serialize(), serialize.NewJSONSerializer().PreKeySignalMessage, serialize.NewJSONSerializer().SignalMessage)
-		if err != nil {
-			logger.Debug("\n\n FAILE DTO MAKE PREKEYSINALMESSAGEFROMYBTES\n\n", err, "\n\n")
-			return err
-		}
-		_, err = s.sessionBuilder.Process(rm)
-		if err != nil {
-			logger.Debug("\n\n FAILE DTO PROCESSSSSS \n\n", err, "\n\n")
-			return err
-		}
-
 		// Step 4: Send first Message
 		logger.Debug("\nHandshake-Dialer\nStep 4: Sending first Message\n")
 		i, err := s.writeMsgInsecure(message.Serialize())
@@ -84,8 +71,32 @@ func (s *signalSession) Handshake(ctx context.Context) (err error) {
 		// TODO FINISH
 
 	} else {
-		/* 		remoteAddr := protocol.NewSignalAddress("listener", 1)
-		 */
+
+		// Step 0: Preparations (including ReadBundle and session.NewBuilder)
+		logger.Debug("\nHandshake-Listener\nStep 0: Preparations (incl. Readbundle and session.newbuilder)\n")
+		remoteAddr := protocol.NewSignalAddress("dialer", 2)
+
+		s.sessionBuilder = *session.NewBuilder(
+			&s.sessionStore, &s.prekeyStore, &s.signedprekeyStore, &s.identityStore,
+			remoteAddr, s.sessionStore.serializer,
+		)
+
+		// Step 1: Receive first message
+		mlen, err := s.readNextInsecureMsgLen()
+		if err != nil {
+			logger.Debug("\nHandshake-Listener\nReturning; Failed to read messageLength!\n", err, "\n")
+			return err
+		}
+
+		hbuf := pool.Get(mlen + 1000)
+		defer pool.Put(hbuf)
+
+		err = s.readNextMsgInsecure(hbuf)
+		if err != nil {
+			logger.Debug("\nHandshake-Listener\nReturning; Failed to read message!\n", err, "\n")
+			return err
+		}
+
 	}
 
 	if s.initiator {
@@ -94,5 +105,5 @@ func (s *signalSession) Handshake(ctx context.Context) (err error) {
 		logger.Debug("\nHandshake-Listener\nFinished Handshake without errors!\n")
 	}
 
-	return errors.New("handshake not implemented")
+	return nil
 }
--- signallibp2p/rw.go
+++ signallibp2p/rw.go
@@ -3,6 +3,12 @@ package signallibp2p
 import (
 	"encoding/binary"
 	"io"
+
+	"github.com/Luca3317/libsignalcopy/logger"
+	"github.com/Luca3317/libsignalcopy/protocol"
+	"github.com/Luca3317/libsignalcopy/serialize"
+	pool "github.com/libp2p/go-buffer-pool"
+	"golang.org/x/crypto/poly1305"
 )
 
 // TODO: Insecure functions copied from noise; might be wrong for signal
@@ -32,11 +38,165 @@ func (s *signalSession) writeMsgInsecure(data []byte) (int, error) {
 	return s.insecureConn.Write(data)
 }
 
-func (s *signalSession) Write(data []byte) (int, error) {
-	return s.writeMsgInsecure(data)
+// right now returns just the length of the buffer
+func (s *signalSession) Reads(buf []byte) (int, error) {
+	return len(buf), s.readNextMsgInsecure(buf)
 }
 
-// right now returns just the length of the buffer
+// Read reads from the secure connection, returning plaintext data in `buf`.
+//
+// Honours io.Reader in terms of behaviour.
 func (s *signalSession) Read(buf []byte) (int, error) {
-	return len(buf), s.readNextMsgInsecure(buf)
+	s.readLock.Lock()
+	defer s.readLock.Unlock()
+
+	// 1. If we have queued received bytes:
+	//   1a. If len(buf) < len(queued), saturate buf, update seek pointer, return.
+	//   1b. If len(buf) >= len(queued), copy remaining to buf, release queued buffer back into pool, return.
+	//
+	// 2. Else, read the next message off the wire; next_len is length prefix.
+	//   2a. If len(buf) >= next_len, copy the message to input buffer (zero-alloc path), and return.
+	//   2b. If len(buf) >= (next_len - length of Authentication Tag), get buffer from pool, read encrypted message into it.
+	//        message directly into the input buffer and return the buffer obtained from the pool.
+	//   2c. If len(buf) < next_len, obtain buffer from pool, copy entire message into it, saturate buf, update seek pointer.
+	if s.qbuf != nil {
+		// we have queued bytes; copy as much as we can.
+		copied := copy(buf, s.qbuf[s.qseek:])
+		s.qseek += copied
+		if s.qseek == len(s.qbuf) {
+			// queued buffer is now empty, reset and release.
+			pool.Put(s.qbuf)
+			s.qseek, s.qbuf = 0, nil
+		}
+		return copied, nil
+	}
+
+	// length of the next encrypted message.
+	nextMsgLen, err := s.readNextInsecureMsgLen()
+	if err != nil {
+		return 0, err
+	}
+
+	// If the buffer is atleast as big as the encrypted message size,
+	// we can read AND  in place.
+	if len(buf) >= nextMsgLen {
+		if err := s.readNextMsgInsecure(buf[:nextMsgLen]); err != nil {
+			return 0, err
+		}
+
+		dbuf, err := s.(buf[:0], buf[:nextMsgLen])
+		if err != nil {
+			return 0, err
+		}
+
+		return len(dbuf), nil
+	}
+
+	// otherwise, we get a buffer from the pool so we can read the message into it
+	// and then  in place, since we're retaining the buffer (or a view thereof).
+	cbuf := pool.Get(nextMsgLen)
+	if err := s.readNextMsgInsecure(cbuf); err != nil {
+		return 0, err
+	}
+
+	if s.qbuf, err = s.(cbuf[:0], cbuf); err != nil {
+		return 0, err
+	}
+
+	// copy as many bytes as we can; update seek pointer.
+	s.qseek = copy(buf, s.qbuf)
+
+	return s.qseek, nil
+}
+
+func (s *signalSession) decrypt(out, ciphertext []byte) ([]byte, error) {
+	signalmessage, err := protocol.NewSignalMessageFromBytes(ciphertext, serialize.NewJSONSerializer().SignalMessage)
+	if err != nil {
+		logger.Debug("\n\nCANNOT ; FAILED TO CREAT SIGNALMESSAGE\n", err, "\n\n")
+		return nil, err
+	}
+
+	ed, err := s.sessionCipher.(signalmessage)
+	if err != nil {
+		logger.Debug("\n\nCANNOT ; FAILED TO DECRPYT\n", err, "\n\n")
+		return nil, err
+	}
+
+	return ed, nil
+}
+
+func (s *signalSession) encrypt(out, plaintext []byte) ([]byte, error) {
+	ciphermessage, err := s.sessionCipher.Encrypt(plaintext)
+	if err != nil {
+		logger.Debug("\n\nCANNOT ENCRYPT; FAILED TO ENCRYPT\n", err, "\n\n")
+		return nil, err
+	}
+
+	out = ciphermessage.Serialize()
+	return ciphermessage.Serialize(), err
+}
+
+// Write encrypts the plaintext `in` data and sends it on the
+// secure connection.
+func (s *signalSession) Write(data []byte) (int, error) {
+	s.writeLock.Lock()
+	defer s.writeLock.Unlock()
+
+	var (
+		written int
+		cbuf    []byte
+		total   = len(data)
+	)
+
+	cbuf = pool.Get(total)
+	defer pool.Put(cbuf)
+
+	ciphertext, err := s.encrypt(cbuf, data)
+	if err != nil {
+		return 0, err
+	}
+
+	return s.insecureConn.Write(data)
+}
+
+// Write encrypts the plaintext `in` data and sends it on the
+// secure connection.
+func (s *signalSession) NoiseWrite(data []byte) (int, error) {
+	s.writeLock.Lock()
+	defer s.writeLock.Unlock()
+
+	var (
+		written int
+		cbuf    []byte
+		total   = len(data)
+	)
+
+	if total < MaxPlaintextLength {
+		cbuf = pool.Get(total + poly1305.TagSize + LengthPrefixLength)
+	} else {
+		cbuf = pool.Get(MaxTransportMsgLength + LengthPrefixLength)
+	}
+
+	defer pool.Put(cbuf)
+
+	for written < total {
+		end := written + MaxPlaintextLength
+		if end > total {
+			end = total
+		}
+
+		b, err := s.encrypt(cbuf[:LengthPrefixLength], data[written:end])
+		if err != nil {
+			return 0, err
+		}
+
+		binary.BigEndian.PutUint16(b, uint16(len(b)-LengthPrefixLength))
+
+		_, err = s.writeMsgInsecure(b)
+		if err != nil {
+			return written, err
+		}
+		written = end
+	}
+	return written, nil
 }
