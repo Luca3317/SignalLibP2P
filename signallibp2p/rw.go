3db611faa965cfc6a32b6563386c1dfdc3a2e797
Luca Weist <lweist3317@gmail.com>
2022-06-04 18:46:03 +0200

    sasa

--- signallibp2p/rw.go
+++ signallibp2p/rw.go
@@ -4,18 +4,20 @@ import (
 	"github.com/Luca3317/libsignalcopy/logger"
 )
 
-const MaxPlaintextLength = 16384
+const MaxPlaintextLength = 32768
 const LengthPrefixLength = 2
 const MaxTransportMsgLength = 0xffff
 
 func (s *signalSession) Read(buf []byte) (int, error) {
 	s.readLock.Lock()
+	s.writeLock.Lock()
 	defer s.readLock.Unlock()
+	defer s.writeLock.Unlock()
 
-	logger.Debug("reading... \n")
+	logger.Debug("reading... ")
 	i, err := s.insecureConn.Read(buf)
 	if err != nil {
-		logger.Debug("FAILED TO READ\n")
+		logger.Debug("FAILED TO READ")
 	} else {
 		logger.Debug("Success; read ", buf, " (", string(buf), ")")
 	}
@@ -27,14 +29,16 @@ func (s *signalSession) Read(buf []byte) (int, error) {
 
 func (s *signalSession) Write(data []byte) (int, error) {
 	s.writeLock.Lock()
+	s.readLock.Lock()
 	defer s.writeLock.Unlock()
+	defer s.readLock.Unlock()
 
 	//	total := len(data)
 
 	logger.Debug("writing ", data, " (", string(data), ") ...")
 	i, err := s.insecureConn.Write(data)
 	if err != nil {
-		logger.Debug("FAILED TO Write\n")
+		logger.Debug("FAILED TO WRITE")
 	} else {
 		logger.Debug("Success;")
 	}
