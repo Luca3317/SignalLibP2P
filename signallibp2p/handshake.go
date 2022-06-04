d6337ea340e99ac72fc4c75667d40147bb27f54f
Luca Weist <lweist3317@gmail.com>
2022-06-04 17:34:50 +0200

    trailing

--- signallibp2p/handshake.go
+++ signallibp2p/handshake.go
@@ -282,7 +282,7 @@ func (s *signalSession) Handshake(ctx context.Context) (err error) {
 
 		logger.Debug("Im leaving my handshake\n")
 		logger.Debug("First sending test message\n")
-		i, err = s.Write([]byte("My test message... wwhats up"))
+		i, err = s.Write(append([]byte("My test message... wwhats up"), 0))
 		if err != nil {
 			logger.Debug("I failed to write for some reason\n")
 			logger.Debug(err)
